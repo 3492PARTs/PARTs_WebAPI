7e189fb567b6fc0c60e71929f733aa502cc2095d
Brandon Duke <bmduke1997@gmail.com>
2020-02-11 08:11:31 -0500

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -138,13 +138,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-'''
+STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     (os.path.join(BASE_DIR, 'staticfiles')),
 )
-'''
+
 
 # URLs
 LOGIN_URL = '/auth/login/'
