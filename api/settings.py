3d9dcae21966d9b794ada06f32cedd27391fa88d
Brandon Duke <bmduke1997@gmail.com>
2020-02-11 07:37:43 -0500

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -140,7 +140,10 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles/')
+STATICFILES_DIRS = (
+    (os.path.join(BASE_DIR, 'static')),
+)
 
 # URLs
 LOGIN_URL = '/auth/login/'
