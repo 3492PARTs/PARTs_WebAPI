de590fefc3443bd090858cb07026ee8c5559fb4a
Brandon <brandon@bduke.dev>
2023-03-10 15:28:26 -0500

    time stamp

--- scripts/notify-users.sh
+++ scripts/notify-users.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 newline=$'\n'
-timestamp=$(date +%s)
+timestamp=$(date)
 output=$(curl https://parts.bduke.dev/public/notify-users/)
 
 echo "$timestamp" "$output" "$newline" >> /home/brandon/PARTs_WebAPI/logs/log-notify-users.txt
