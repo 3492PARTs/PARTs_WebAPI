54be633338092c577b8f2e27e4f464505834c853
Brandon <brandon@bduke.dev>
2024-02-24 09:08:58 -0500

    update jwt

--- user/urls.py
+++ user/urls.py
@@ -10,7 +10,7 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
-    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     #TokenRefreshView.as_view()
     path('user-data/', UserData.as_view()),
     path('user-links/', UserLinksView.as_view()),
