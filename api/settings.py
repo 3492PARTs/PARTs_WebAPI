0c201793550baad6df09c554a4b9a14bffb0a8f7
Brandon Duke <brandon@bduke.dev>
2024-04-09 09:30:54 -0400

    disable collect static

--- api/settings.py
+++ api/settings.py
@@ -162,6 +162,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+DISABLE_COLLECTSTATIC = 1
+
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_URL = '/static/'
 
