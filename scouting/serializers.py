92003a918dcba70acf5974bbd92999412d5209e3
Brandon <brandon@bduke.dev>
2024-04-28 20:28:39 -0400

    scout schedule

--- scouting/serializers.py
+++ scouting/serializers.py
@@ -78,13 +78,13 @@ class TeamSerializer(serializers.Serializer):
 
 
 class UserSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+    id = serializers.IntegerField()
     username = serializers.CharField()
     email = serializers.CharField()
     first_name = serializers.CharField()
     last_name = serializers.CharField()
     is_active = serializers.BooleanField()
-    phone = serializers.CharField()
+    phone = serializers.CharField(allow_blank=True)
     phone_type_id = serializers.IntegerField(required=False, allow_null=True)
 
 
@@ -102,20 +102,20 @@ class ScheduleSerializer(serializers.Serializer):
 
 class ScoutFieldScheduleSerializer(serializers.Serializer):
     scout_field_sch_id = serializers.IntegerField()
-    event_id = serializers.IntegerField(read_only=True)
+    event_id = serializers.IntegerField()
     st_time = serializers.DateTimeField()
     end_time = serializers.DateTimeField()
-    notification1 = serializers.BooleanField(read_only=True)
-    notification2 = serializers.BooleanField(read_only=True)
-    notification3 = serializers.BooleanField(read_only=True)
+    notification1 = serializers.BooleanField()
+    notification2 = serializers.BooleanField()
+    notification3 = serializers.BooleanField()
 
-    red_one_id = UserSerializer(required=False, allow_null=True, read_only=True)
-    red_two_id = UserSerializer(required=False, allow_null=True, read_only=True)
-    red_three_id = UserSerializer(required=False, allow_null=True, read_only=True)
-    blue_one_id = UserSerializer(required=False, allow_null=True, read_only=True)
-    blue_two_id = UserSerializer(required=False, allow_null=True, read_only=True)
-    blue_three_id = UserSerializer(required=False, allow_null=True, read_only=True)
-    scouts = serializers.CharField(read_only=True)
+    red_one_id = UserSerializer(required=False, allow_null=True)
+    red_two_id = UserSerializer(required=False, allow_null=True)
+    red_three_id = UserSerializer(required=False, allow_null=True)
+    blue_one_id = UserSerializer(required=False, allow_null=True)
+    blue_two_id = UserSerializer(required=False, allow_null=True)
+    blue_three_id = UserSerializer(required=False, allow_null=True)
+    scouts = serializers.CharField()
 
 
 class ScheduleTypeSerializer(serializers.Serializer):
