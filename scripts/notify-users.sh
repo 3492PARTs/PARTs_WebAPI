2f05c76883971246ec936c9c1ce764541e469145
Brandon Duke <brandon@bduke.dev>
2024-11-05 13:18:29 -0500

    fix path

--- scripts/notify-users.sh
+++ scripts/notify-users.sh
@@ -4,8 +4,8 @@ newline=$'\n'
 timestamp=$(date)
 output=$(curl https://api.parts3492.org/alerts/run/)
 
-echo "$timestamp" "$output" "$newline" >> /domains/api.parts3492.org/code/log/log-notify-users.txt
+echo "$timestamp" "$output" "$newline" >> /domains/api.parts3492.org/code/logs/log-notify-users.txt
 
 output=$(curl https://api.parts3492.org/alerts/send/)
 
-echo "$timestamp" "$output" "$newline" >> /domains/api.parts3492.org/code/log/log-notify-users.txt
+echo "$timestamp" "$output" "$newline" >> /domains/api.parts3492.org/code/logs/log-notify-users.txt
