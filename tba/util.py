9a2f2d507593ddffe1f55a79fa62c1ae39e6ad21
Brandon Duke <brandon@bduke.dev>
2025-03-28 09:57:08 -0400

    cast message data

--- tba/util.py
+++ tba/util.py
@@ -381,7 +381,7 @@ def save_tba_match(tba_match):
 def save_message(message):
     msg = Message(
         message_type=message["message_type"],
-        message_data=message["message_data"][:4000],
+        message_data=str(message["message_data"])[:4000],
     )
     msg.save()
     return msg
