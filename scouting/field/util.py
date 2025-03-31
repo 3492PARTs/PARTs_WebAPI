eb39253e921943dbb7744aafec0adb3517ba29bf
Brandon Duke <brandon@bduke.dev>
2025-03-31 13:49:37 -0400

    up limit

--- scouting/field/util.py
+++ scouting/field/util.py
@@ -181,7 +181,7 @@ def get_responses(pg=1, team=None, user=None, after_scout_field_id=None):
         .order_by("-time", "-id")
     )
 
-    paginator = Paginator(scout_field_responses, 10)
+    paginator = Paginator(scout_field_responses, 20)
     try:
         scout_field_responses = paginator.page(pg)
     except PageNotAnInteger:
