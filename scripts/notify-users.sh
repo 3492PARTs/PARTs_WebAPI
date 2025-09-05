1c0d52dfe73f1410f473d686b2d790f033229984
Brandon Duke <brandon@bduke.dev>
2025-09-05 15:05:18 -0400

    fix call

--- scripts/notify-users.sh
+++ scripts/notify-users.sh
@@ -2,7 +2,7 @@
 
 newline=$'\n'
 timestamp=$(date)
-output=$(curl DEPLOY_URL/alerts/run/)
+output=$(curl DEPLOY_URL/alerts/stage/)
 
 echo "$timestamp" "$output" "$newline" >> DEPLOY_PATH/logs/log-notify-users.txt
 
