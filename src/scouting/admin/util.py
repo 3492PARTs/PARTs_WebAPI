4ab272a2779587a0c63970c88a2c31b1058316d8
Brandon <brandon@bduke.dev>
2026-06-18 22:22:41 -0400

    delete

--- src/scouting/admin/util.py
+++ src/scouting/admin/util.py
@@ -898,3 +898,26 @@ def save_user_season(user_season: dict[str, Any]) -> UserSeason:
     us.save()
     return us
 
+def save_user_seasons(user_seasons: list[dict[str, Any]]) -> list[UserSeason]:
+    """
+    Save or update multiple user season records.
+
+    Args:
+        user_seasons: List of dictionaries containing user season data (id, user_id, season_id, void_ind)
+                If id is present in a dictionary, updates existing user season; otherwise creates new one
+                Using the list of provided user seasons to determine which user seasons to delete (those not in the list)
+
+    Returns:
+        List of created or updated UserSeason objects
+    """
+    
+    user_season_ids = [us["id"] for us in user_seasons if us.get("id") is not None]
+    user_ids = [us["user"]["id"] for us in user_seasons if us.get("id") is not None]
+    
+    # Set void_ind to 'y' for any existing user seasons not in the provided list
+    UserSeason.objects.filter(~Q(id__in=user_season_ids) & Q(user__id__in=user_ids) & Q(void_ind='n')).update(void_ind='y')
+
+    result = []
+    for user_season in user_seasons:
+        result.append(save_user_season(user_season))
+    return result
