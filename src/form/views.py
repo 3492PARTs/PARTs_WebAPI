08127e48471c08b7c13a4a0efb1e59f5fe42eafc
Brandon <brandon@bduke.dev>
2026-07-24 20:19:13 -0400

    fix error logging

--- src/form/views.py
+++ src/form/views.py
@@ -49,7 +49,7 @@ class QuestionView(APIView):
                 app_url + self.endpoint,
                 -1,
                 e,
-                request,
+                str(request),
             )
 
     def post(self, request, format=None):
